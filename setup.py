e51b140aab5fe1a1c8f5367e797ca765277d961d
Luke Robot Hospadaruk <Luke.Hospadaruk@ithaka.org>
2014-10-27 11:37:36 -0400

    added setup script

---
+++ setup.py
@@ -0,0 +1,2 @@
+from distutils.core import setup
+setup(name='sqlite-object', version="0.1", description="sqlite-backed collection objects", author="Luke Hospadaruk", url="http://hospadaruk.org", packages=['sqlite_object'])
